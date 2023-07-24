ab10ddd1140f441754e0b0d3296bf0124d88a561
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-07-24 13:58:54 +0300

    Fix: No env warning

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import 'dotenv/config'
+if (!process.env.DB_HOST || !process.env.DB_NAME || !process.env.FRONTEND_URL || !process.env.DISCORD_TOKEN) throw new Error('Missing env variables')
+
 import { createApp } from './utils/app'
 import client from './utils/client'
 import './utils/db'
-
-if (!process.env.DB_HOST || !process.env.DB_NAME || !process.env.FRONTEND_URL || !process.env.DISCORD_TOKEN) {
-  console.log(process.env)
-  throw new Error('Missing env variables')
-}
 
 const main = () => {
   const PORT = process.env.PORT || 3000
