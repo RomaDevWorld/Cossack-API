e21c58d627b130b5a01dc0499bef1920ab36e3b4
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-07-24 14:36:16 +0300

    Update: mongodb.ts

--- src/utils/mongodb.ts
+++ src/utils/mongodb.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'
-import { config } from 'dotenv'
-config()
+import 'dotenv/config'
 
 mongoose
   .connect(process.env.MONGOURL as string)
-  .then(() => console.log(`[MongoDB] Connected to ${process.env.DB_HOST}@${process.env.DB_NAME}`))
+  .then(() => console.log(`[MongoDB] Connected to MongoDB`))
   .catch((err) => console.error(err))
+
 export default mongoose
