69370df5a7698f7ea127b8e4a5891b46b8775da8
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-07-24 14:36:30 +0300

    Update: Modules schema

--- src/schemas/Modules.ts
+++ src/schemas/Modules.ts
@@ -1,32 +1,75 @@
 import { SchemaTypes, Schema, model } from 'mongoose'
-import { ModulesI } from '../../types'
 
-const ModulesSchema = new Schema<ModulesI>({
+export const logTypes = {
+  messageDelete: { type: SchemaTypes.Boolean, default: false },
+  messageUpdate: { type: SchemaTypes.Boolean, default: false },
+  guildMemberAdd: { type: SchemaTypes.Boolean, default: false },
+  guildMemberRemove: { type: SchemaTypes.Boolean, default: false },
+  guildMemberNicknameUpdate: { type: SchemaTypes.Boolean, default: false },
+  guildMemberRolesUpdate: { type: SchemaTypes.Boolean, default: false },
+  guildMemberTimeout: { type: SchemaTypes.Boolean, default: false },
+  voiceStateUpdate: { type: SchemaTypes.Boolean, default: false },
+  guildBanRemove: { type: SchemaTypes.Boolean, default: false },
+  guildBanAdd: { type: SchemaTypes.Boolean, default: false },
+  guildMemberReport: { type: SchemaTypes.Boolean, default: false },
+}
+
+const ModulesSchema = new Schema({
   guildId: {
     type: SchemaTypes.String,
     required: true,
     unique: true,
   },
-  logChannel: {
-    type: SchemaTypes.String,
-    required: true,
-    unique: true,
-    default: null,
+  log: {
+    channel: {
+      type: SchemaTypes.String,
+      unique: true,
+      default: null,
+    },
+    types: logTypes,
   },
   lobby: {
     channel: {
       type: SchemaTypes.String,
-      required: true,
-      unique: true,
-      default: null,
-    },
-    category: {
-      type: SchemaTypes.String,
-      required: true,
       unique: true,
       default: null,
     },
   },
+  counter: {
+    channelId: {
+      type: SchemaTypes.String,
+      unique: true,
+      default: null,
+    },
+    label: {
+      type: SchemaTypes.String,
+      default: null,
+    },
+  },
+  roles: {
+    autorole: {
+      type: SchemaTypes.String,
+      default: null,
+    },
+    restore: {
+      status: { type: SchemaTypes.Boolean, default: false },
+      expireTime: {
+        type: SchemaTypes.Number,
+        default: 86400000 * 7, // 7 days
+      },
+    },
+  },
+  tickets: [
+    {
+      channelId: {
+        type: SchemaTypes.String,
+      },
+      messageId: {
+        type: SchemaTypes.String,
+      },
+      allowedRoles: [String],
+    },
+  ],
 })
 
 export default model('modules', ModulesSchema)
--- types.d.ts
+++ types.d.ts
@@ -6,13 +6,3 @@ export type PartialGuild = {
   permissions: string
   features: string[]
 }
-
-export interface ModulesI {
-  guildId: string
-  logChannel: string
-  owner: string
-  lobby: {
-    channel: string
-    category: string
-  }
-}
