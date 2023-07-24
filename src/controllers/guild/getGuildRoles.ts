d3f4273b03f88e62653d23ca3398cff3b5240bec
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-07-24 19:31:28 +0300

    Add: 2 router endpoints
    guilds/[guildId]/roles - returns json object of server roles
    guilds/[guildId]/member - returns json object of member

---
+++ src/controllers/guild/getGuildRoles.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+import client from '../../utils/client'
+
+export default async function (req: Request, res: Response) {
+  try {
+    const { id } = req.params
+    const guild = await client.guilds.fetch(id)
+    const roles = await guild.roles.fetch()
+
+    return res.status(200).send(roles)
+  } catch (err) {
+    console.error(err)
+    res.status(500)
+  }
+}
--- src/routes/guilds/index.ts
+++ src/routes/guilds/index.ts
@@ -5,6 +5,8 @@ import updateGuildModules from '../../controllers/guild/updateGuildModules'
 import getGuildModules from '../../controllers/guild/getGuildModules'
 import getGuildChannels from '../../controllers/guild/getGuildChannels'
 import isGuildAdmin from '../../middlewares/isGuildAdmin'
+import getGuildRoles from '../../controllers/guild/getGuildRoles'
+import getGuildMembers from '../../controllers/guild/getGuildMembers'
 
 const router = Router()
 
@@ -13,6 +15,8 @@ router.get('/', getGuilds)
 router.get('/:id', getOneGuild)
 
 router.get('/:id/channels', isGuildAdmin, getGuildChannels)
+router.get('/:id/roles', isGuildAdmin, getGuildRoles)
+router.get('/:id/members', isGuildAdmin, getGuildMembers)
 
 //Configuration
 router.get('/:id/admin/modules/', isGuildAdmin, getGuildModules)
