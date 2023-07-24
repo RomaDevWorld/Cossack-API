0b41dddc4c9d97ea28c99799287fa105fe4539f8
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-07-24 21:42:54 +0300

    Update: getGuildModules.ts
    It will now return default value if null

--- src/controllers/guild/getGuildModules.ts
+++ src/controllers/guild/getGuildModules.ts
@@ -4,7 +4,11 @@ import guildModules from '../../schemas/Modules'
 export default async function (req: Request, res: Response) {
   try {
     const { id: guildId } = req.params
-    const dbValue = await guildModules.findOne({ guildId })
+    const dbValue = await guildModules.findOneAndUpdate(
+      { guildId },
+      { guildId },
+      { new: true, upsert: true }
+    )
 
     res.status(200).json(dbValue)
   } catch (error) {
