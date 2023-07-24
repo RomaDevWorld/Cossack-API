c292006f9c3c6b2942b9c8b7100a3d4f3c3eccf2
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-07-24 14:37:32 +0300

    Update: remove console statement

--- src/controllers/guild/updateGuildModules.ts
+++ src/controllers/guild/updateGuildModules.ts
@@ -5,8 +5,6 @@ export default async function (req: Request, res: Response) {
   const { id: guildId } = req.params
   const modules: typeof Modules = req.body
 
-  console.log(req.body)
-
   await Modules.findOneAndUpdate({ guildId }, modules, { upsert: true })
 
   return res.sendStatus(200)
