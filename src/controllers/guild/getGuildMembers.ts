980283066c8846efa570813a69b29d9de4a1cb62
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-07-27 12:44:02 +0300

    Update getGuildMembers.ts

--- src/controllers/guild/getGuildMembers.ts
+++ src/controllers/guild/getGuildMembers.ts
@@ -7,7 +7,7 @@ export default async function (req: Request, res: Response) {
     const guild = await client.guilds.fetch(id)
     const members = await guild.members.fetch()
 
-    return res.status(200).send(members)
+    return res.status(200).json(members)
   } catch (err) {
     console.error(err)
     res.status(500)
