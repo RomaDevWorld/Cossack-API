b0a1b8da83693c5ea86973b5237abf0d18e81454
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-08-10 23:06:49 +0300

    Update: app.ts

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -8,8 +8,8 @@ import MongoStore from 'connect-mongo'
 import rateLimit from 'express-rate-limit'
 
 const limiter = rateLimit({
-  windowMs: 60 * 1000,
-  max: 20,
+  windowMs: 15 * 1000,
+  max: 10,
   message: 'You are being rate limited',
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
