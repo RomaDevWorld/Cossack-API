59fd1adbaf2b50546cfde343b6192a9ded9bf0d5
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-08-23 22:29:00 +0300

    fix: Add catch block in getUserInfoService.ts

--- src/services/auth/getUserInfoService.ts
+++ src/services/auth/getUserInfoService.ts
@@ -5,9 +5,13 @@ export async function getUserInfoService(id: string) {
   const user = await User.findById(id)
   if (!user) throw new Error('User not found')
 
-  return axios.get('https://discord.com/api/v10/users/@me', {
-    headers: {
-      Authorization: `Bearer ${user.accessToken}`,
-    },
-  })
+  const response = axios
+    .get('https://discord.com/api/v10/users/@me', {
+      headers: {
+        Authorization: `Bearer ${user.accessToken}`,
+      },
+    })
+    .catch((err) => err)
+
+  return response
 }
