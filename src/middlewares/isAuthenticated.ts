6536e60a29baad35026e5b817e26707d05b44f8b
RomaDevWorld <50528338+RomaDevWorld@users.noreply.github.com>
2023-08-28 21:51:05 +0300

    fix: validate authToken

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,7 +1,23 @@
 import { NextFunction, Request, Response } from 'express'
+import { UserI } from '../schemas/User'
 
-const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
-  req.user ? next() : res.sendStatus(401)
+const isAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
+  if (!req.user) return res.sendStatus(401)
+
+  const user = req.user as UserI
+
+  try {
+    const response = await fetch('https://discord.com/api/v10/users/@me', {
+      headers: {
+        Authorization: `Bearer ${user.accessToken}`,
+      },
+    })
+
+    response.ok ? next() : res.sendStatus(401)
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
 }
 
 export default isAuthenticated
